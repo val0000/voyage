089c1b7220c6b7a01dd28a1bdf8b9ddca800f611
Zaerowolf <ldarruau@gmail.com>
2017-05-22 17:04:41 +0200

    plan page travel

--- src/app/ihm/travel.component.ts
+++ src/app/ihm/travel.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   moduleId : module.id,
   selector: 'my-travel',
   templateUrl: 'travel.component.html',
+  styleUrls: ['travel.component.css'],
 })
 
 export class TravelComponent {
