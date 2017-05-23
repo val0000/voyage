0170ab590a8b97ca5df433e95cd5563ca2005350
val0901 <Formation@FR-D23898.groupinfra.com>
2017-05-23 15:23:40 +0200

    page article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,18 @@ import { AppComponent }  from './ihm/app.component';
 import { DashboardComponent } from './ihm/dashboard.component';
 import { TravelComponent } from './ihm/travel.component';
 import { ContactComponent } from './ihm/contact.component';
+import { ArticleComponent } from './ihm/article.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule, RouterModule.forRoot([
                 {path: 'dashboard', component: DashboardComponent},
                 {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
                 {path: 'travel', component: TravelComponent},
-                {path: 'contact', component: ContactComponent}
+                {path: 'contact', component: ContactComponent},
+                {path: 'article', component: ArticleComponent}
                 ])],
                 
-          declarations: [ AppComponent, DashboardComponent, TravelComponent, ContactComponent ],
+          declarations: [ AppComponent, DashboardComponent, TravelComponent, ContactComponent, ArticleComponent ],
           bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
