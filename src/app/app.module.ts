dc6894c22573d02c35f1caeb0eac97829a013c01
Zaerowolf <ldarruau@gmail.com>
2017-05-23 14:42:50 +0200

    ajout contact, routes/html 123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,17 @@ import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './ihm/app.component';
 import { DashboardComponent } from './ihm/dashboard.component';
 import { TravelComponent } from './ihm/travel.component';
+import { ContactComponent } from './ihm/contact.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule, RouterModule.forRoot([
                 {path: 'dashboard', component: DashboardComponent},
                 {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-                {path: 'travel', component: TravelComponent}
+                {path: 'travel', component: TravelComponent},
+                {path: 'contact', component: ContactComponent}
                 ])],
                 
-          declarations: [ AppComponent, DashboardComponent, TravelComponent ],
+          declarations: [ AppComponent, DashboardComponent, TravelComponent, ContactComponent ],
           bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
