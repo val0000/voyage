f530b7a91119cdd1e3fad855f6621943e69c640d
Doing33 <Formation@FR-D23892.groupinfra.com>
2017-05-24 11:08:41 +0200

    click et filtre

--- src/app/ihm/travel.component.ts
+++ src/app/ihm/travel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Voyage, Saison,Localisation, Activite } from './voyage';
+import { BOUCHONVOYAGES } from './mock-voyage';
 
 @Component({
   moduleId : module.id,
@@ -9,4 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TravelComponent {
+
+    // flitre choisi par l'utilisateur
+    saison: Saison;
+    activite: Activite;  
+    localisation: Localisation;
+
+    // tous les voyages
+    voyages: Voyage[] = BOUCHONVOYAGES;
+
+    // les voyages filtrés
+    voyagesFiltres: Voyage[];
+
+    clickSaison(saison: Saison) {
+        this.saison = saison;
+    }
+        
+    clickLocalisation(localisation: Localisation) {
+        this.localisation = localisation;
+    }   
+        
+    clickActivite(activite: Activite) {
+        this.activite = activite;
+    }
+    
+    filtre(): string {
+        return `saison: ${Saison[this.saison]} activite: ${this.activite} localisation: ${this.localisation}`;
+    }
+
 }
