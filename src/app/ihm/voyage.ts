3a7b201c2e3319d4965d8e3e21f6aef318076071
Doing33 <Formation@FR-D23892.groupinfra.com>
2017-05-24 09:13:39 +0200

    création voyage.ts et de mock-voyage.ts

---
+++ src/app/ihm/voyage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-voyage',
+    //templateUrl: 'voyage.component.html',
+})
+export class VoyageComponent  {
+   }
